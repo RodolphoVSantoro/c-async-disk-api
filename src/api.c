8bf93e2ee4248911ef877913129bbbdf882cbfed
rodolphvs <rodolphovs@gmail.com>
2025-05-30 16:45:35 -0300

    fix: refactor - decrease identation

--- src/api.c
+++ src/api.c
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
     while (true) {
         readySockets = currentSockets;
 
-        // Wait for an activity on one of the sockets
+        // Wait for activity on any socket
         if (select(FD_SETSIZE, &readySockets, NULL, NULL, NULL) < 0) {
             printf("Select failed");
             return ERROR;
@@ -51,33 +51,37 @@ int main(int argc, char* argv[]) {
 
         // Check all sockets for activity
         for (int socket = 0; socket < FD_SETSIZE; socket++) {
-            if (FD_ISSET(socket, &readySockets)) {
-                // Accept new connection
-                if (socket == serverSocket) {
-                    struct sockaddr_in clientAddress;
-                    socklen_t clientAddressSize = sizeof(clientAddress);
-                    int clientSocket = accept(serverSocket, (SA*)&clientAddress, &clientAddressSize);
-                    FD_SET(clientSocket, &currentSockets);
+            // If the socket is not ready, continue to the next one
+            if (!FD_ISSET(socket, &readySockets)) {
+                continue;
+            }
+            
+            // Accept new connection and continue to the next socket
+            if (socket == serverSocket) {
+                struct sockaddr_in clientAddress;
+                socklen_t clientAddressSize = sizeof(clientAddress);
+                int clientSocket = accept(serverSocket, (SA*)&clientAddress, &clientAddressSize);
+                FD_SET(clientSocket, &currentSockets);
+                continue;
+            }
+
+            // Handle client request
+            int clientSocket = socket;
+            char request[SOCKET_READ_SIZE];
+            int bytesRead = recv(clientSocket, request, sizeof(request), SEND_DEFAULT);
+
+            if (bytesRead >= 1 && bytesRead < SOCKET_READ_SIZE) {
+                request[bytesRead] = '\0';
+                int sentResult = handleRequest(request, bytesRead, clientSocket);
+                if (sentResult == ERROR) {
+                    log("{ Error sending response }\n");
                 } else {
-                    // Handle client request
-                    int clientSocket = socket;
-                    char request[SOCKET_READ_SIZE];
-                    int bytesRead = recv(clientSocket, request, sizeof(request), SEND_DEFAULT);
-
-                    if (bytesRead >= 1 && bytesRead < SOCKET_READ_SIZE) {
-                        request[bytesRead] = '\0';
-                        int sentResult = handleRequest(request, bytesRead, clientSocket);
-                        if (sentResult == ERROR) {
-                            log("{ Error sending response }\n");
-                        } else {
-                            log("{ Request handled }\n");
-                        }
-                    }
-
-                    close(clientSocket);
-                    FD_CLR(clientSocket, &currentSockets);
+                    log("{ Request handled }\n");
                 }
             }
+
+            close(clientSocket);
+            FD_CLR(clientSocket, &currentSockets);
         }
     }
 
